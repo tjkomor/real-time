dd3d6c55efc617e475b61c596391d6c19a7812aa
Tyler Komoroske <tjkomor@clemson.edu>
2016-03-06 22:32:02 -0700

    set up initial main handlebar view set up basic server

--- server.js
+++ server.js
@@ -1,61 +1,24 @@
 const http = require('http');
 const express = require('express');
-const socketIo = require ('socket.io');
 const app = express();
+const _ = require('lodash');
+const exphbs  = require('express-handlebars');
 
-app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-var server = http.createServer(app)
-                 .listen(port, function() {
-                   console.log('Listening on port ' + port + '.');
-                 });
-
-const io = socketIo(server);
-
-var votes = {};
-
-function countVotes(votes) {
-  var voteCount = {
-    A: 0,
-    B: 0,
-    C: 0,
-    D: 0
-  };
-
-  for (vote in votes) {
-    voteCount[votes[vote]]++
-  }
-  return voteCount;
-}
-
-io.on('connection', function(socket) {
-
-  console.log('A user has connected.', io.engine.clientsCount);
-
-  io.sockets.emit('usersConnected', io.engine.clientsCount);
-
-  socket.emit('statusMessage', 'You have connected.');
-
-  socket.on('message', function(channel, message) {
-    if (channel === 'voteCast') {
-      votes[socket.id] = message;
-      socket.emit('currentChoice', message);
-      socket.emit('voteCount', countVotes(votes));
-    }
-  });
-
-  socket.on('disconnect', function() {
-    console.log('A user has disconnected.', io.engine.clientsCount);
-    delete votes[socket.id];
-    socket.emit('voteCount', countVotes(votes));
-    io.sockets.emit('usersConnected', io.engine.clientsCount);
-  });
+const server = http.createServer(app)
+.listen(port, function(){
+  console.log('Listening on port ' + port + '.');
 });
 
-module.exports = server;
+const socketIo = require('socket.io');
+const io = socketIo(server);
